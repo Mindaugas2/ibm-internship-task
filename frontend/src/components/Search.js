29015c44594897408b9a7a0c252ff4a656d59c21
Julius <jzabulenas@protonmail.com>
2022-08-17 19:16:28 +0300

    add input field validation

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,19 +4,23 @@ import Form from 'react-bootstrap/Form';
 import { useForm } from "react-hook-form";
 
 export default function Search() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3">
                 <Form.Label>Type in the company's symbol</Form.Label>
+                {errors.symbol?.type === "required" && <p>This field is required</p>}
+                {errors.symbol?.type === "maxLength" && <p>35 is the maximum amount of characters you may use</p>}
+                {errors.symbol?.type === "pattern" && <p>You can only input letters and spaces</p>}
                 <Form.Control
                     placeholder="Enter symbol"
                     {...register("symbol",
                         {
                             required: true,
-                            maxLength: 20
+                            maxLength: 35,
+                            pattern: /^[a-zA-Z\s]*$/
                         })
                     }
                 />
