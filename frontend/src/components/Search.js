0d5533648a92f603ee9411bf7b050b830208c389
Julius <jzabulenas@protonmail.com>
2022-08-17 17:33:47 +0300

    add React Hook Form to project

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
 
 export default function Search() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3">
                 <Form.Label>Type in the company's symbol</Form.Label>
-                <Form.Control type="text" placeholder="Enter symbol" />
+                <Form.Control
+                    placeholder="Enter symbol"
+                    {...register("symbol",
+                        {
+                            required: true,
+                            maxLength: 20
+                        })
+                    }
+                />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button
+                variant="primary"
+                type="submit"
+            >
                 Submit
             </Button>
         </Form>
