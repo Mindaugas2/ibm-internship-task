6678274efedab2c2d060fcf96f51b5d1398264ab
Julius <jzabulenas@protonmail.com>
2022-08-22 19:41:50 +0300

    clean code up a bit

--- frontend/src/components/CompanyResults.js
+++ frontend/src/components/CompanyResults.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import CompanyGraph from './CompanyGraph';
 import CompanyInfo from './CompanyInfo';
-import { Routes, Route } from 'react-router-dom';
 
 export default function CompanyResults({ symbol, dateFrom, dateTo }) {
-    const dateFromHere = dateFrom;
-    const dateToHere = dateTo;
-
     return (
-        // <Routes>
-        //     <Route
-        //         path='/'
-        //         element={<CompanyInfo
-        //             symbol={symbol}
-        //         />}
-        //     />
-        //     <Route
-        //         path='/chart'
-        //         element={
-        //             <CompanyGraph
-        //                 symbol={symbol}
-        //                 dateFrom={dateFromHere}
-        //                 dateTo={dateToHere}
-        //             />}
-        //     />
-        // </Routes>
         <>
             <CompanyInfo
                 symbol={symbol}
